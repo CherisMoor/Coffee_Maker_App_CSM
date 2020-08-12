b4de48c5790555b52f475a3fc9479c5a7daeaa4f
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-08-12 09:03:11 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompTwoComponent } from './comp-two/comp-two.component';
+import { CompOneComponent } from './comp-one/comp-one.component';
+import { ListComponent } from './list/list.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompTwoComponent,
+    CompOneComponent,
+    ListComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
